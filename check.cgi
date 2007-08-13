910a7562bc3b59eb2e5dfc2d2af77358838883bf
Simon Morgan <sjm@spamcop.net>
2007-08-13 15:50:10 +0100

    Initial import.

---
+++ check.cgi
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import cgi
+from os import environ as env
+
+myip = "127.0.0.1"
+
+if __name__ == "__main__":
+    form = cgi.FieldStorage()
+
+    print 'Content-type: text/plain\n'
+
+    if env['REMOTE_ADDR'] == myip:
+        print "junk"
+    elif env.has_key('HTTP_X_FORWARDED_FOR') and env['HTTP_X_FORWARDED_FOR'] == myip:
+        print "junk"
+    else:
+        if form.has_key("ppcheck"):
+            print "ok"
+        else:
+            print "junk"
---
+++ ppcheck.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+import threading
+import time
+import urllib
+
+class ProxyScanner(threading.Thread):
+    def __init__(self, proxy):
+        threading.Thread.__init__(self)
+        self.proxy = proxy
+    def run(self):
+        try:
+            d = urllib.urlencode({ "ppcheck": "ppcheck"}) # Check for POST support.
+            u = urllib.urlopen(checker, d, { "http": "http://" + self.proxy})
+        except KeyboardInterrupt:
+            raise
+        except:
+            return
+        else:
+            result = u.readline().strip()
+            if result == "ok":
+                print self.proxy
+
+checker = "http://www.example.net/check.cgi" # Use a hostname as some proxies don't do DNS.
+numthreads = 100
+
+if len(sys.argv) < 2:
+    sys.exit()
+
+try:
+    f = open(sys.argv[1])
+except:
+    print "Couldn't open %s." % sys.argv[1]
+    sys.exit()
+
+socket.setdefaulttimeout(20)
+
+try:
+    for l in f:
+        while threading.activeCount() == numthreads:
+            time.sleep(2)
+        thread = ProxyScanner(l.strip())
+        thread.start()
+except KeyboardInterrupt:
+    sys.exit()
+
+f.close()
